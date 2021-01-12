db6a7301330faff2dbafa3be8131f20059219f87
Jan Razny <exploit.the.machine@protonmail.com>
2021-01-12 17:22:24 +0100

    .

--- app/src/app.module.ts
+++ app/src/app.module.ts
@@ -7,7 +7,21 @@ import { UsersModule } from "./users/users.module";
 @Module({
   imports: [
     ConfigModule.forRoot({envFilePath:['.env.local', '.env'], isGlobal:true }),
-    TypeOrmModule.forRoot(),
+    TypeOrmModule.forRoot({
+      "name": "default",
+      "type": "mariadb",
+      "host": process.env.TYPEORM_HOST || "maria",
+      "port": 3306,
+      "username": "testuser",
+      "password": "testpassword",
+      "database": "testdatabase",
+      "entities": ["**/*.entity{ .ts,.js}"],
+      "synchronize": false,
+      "migrations": ["dist/migrations/*{.ts,.js}"],
+      "migrationsTableName": "migrations_typeorm",
+      "migrationsRun": true,
+      "autoLoadEntities": true
+    }),
     UsersModule],
   controllers: [],
   providers: [],
